448ca033a0e4c17cc911a1c6eed995cadcd2bbb0
Nawaf Ishaq <Nawaf_fahad07@hotmail.com>
2024-03-02 19:01:42 +0300

    Add project files.

---
+++ src/TodoApp.Application/TodoAppService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace TodoApp
+{
+    public class TodoAppService : TodoAppAppService, ITodoAppService
+    {
+        private readonly IRepository<TodoItem, Guid> _todoItemRepository;
+
+        public TodoAppService(IRepository<TodoItem, Guid> todoItemRepository)
+        {
+            _todoItemRepository = todoItemRepository;
+        }
+
+        // TODO: Implement the methods here...
+
+        public async Task<List<TodoItemDto>> GetListAsync()
+        {
+            var items = await _todoItemRepository.GetListAsync();
+
+            return items.Select(i => new TodoItemDto { Id = i.Id, Text = i.Text }).ToList();
+        }
+
+        public async Task<TodoItemDto> CreateAsync(string text)
+        {
+        
+                var item = await _todoItemRepository.InsertAsync(new TodoItem { Text = text });
+                return new TodoItemDto { Id = item.Id, Text = item.Text };
+
+           
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+           await _todoItemRepository.DeleteAsync(id);
+        }
+    }
+}
---
+++ src/TodoApp.HttpApi/TodoAppApi.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApp.Controllers;
+
+namespace TodoApp
+{
+    public class TodoAppApi : TodoAppController
+    {
+        private readonly ITodoAppService _todoAppService;
+        public TodoAppApi(ITodoAppService todoAppService)
+        {
+            _todoAppService = todoAppService;
+        }
+
+        [HttpGet]
+        public async Task<List<TodoItemDto>> GetTodoItems()
+        {
+            var result = await _todoAppService.GetListAsync();
+            return result;
+        }
+
+        [HttpPost]
+        public async Task<TodoItemDto> PostTodoItem(string text)
+        {
+            var result = await _todoAppService.CreateAsync(text);
+            return result;
+        }
+
+        [HttpDelete]
+        public async Task RemoveTodoItem(Guid id)
+        {
+            await _todoAppService.DeleteAsync(id);
+            
+        }
+
+
+
+
+    }
+}
